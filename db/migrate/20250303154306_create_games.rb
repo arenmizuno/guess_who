5f0c58746363f59ba213339c9a2f1aa13b44adac
Aren Mizuno <166631743+arenmizuno@users.noreply.github.com>
2025-03-04 22:51:09 +0000

    Done

--- db/migrate/20250303154306_create_games.rb
+++ db/migrate/20250303154306_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[7.1]
   def change
     create_table :games do |t|
       t.integer :player_id  # Single player controlling both roles
-      t.string  :status, default: "pending"  # Status of the game (pending, in progress, finished)
+      t.string  :status, default: "pending"  # Status of the game 
       t.string  :current_turn, default: "player1"  # Track whose turn it is ("player1" or "player2")
       t.integer :elected_1  # Character ID chosen by Player 1
       t.integer :elected_2  # Character ID chosen by Player 2
